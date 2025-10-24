1a57c6f30b897de62c89970f4c72372fa123379c
SambhavChhibber <149156793+SambhavChhibber@users.noreply.github.com>
2025-10-24 15:53:42 +0530

    server.js

--- mongo/server.js
+++ mongo/server.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-// Middleware
+
 app.use(bodyParser.json());
 
-// Connect to MongoDB
+
 mongoose.connect('mongodb://127.0.0.1:27017/mydatabase', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,12 +16,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mydatabase', {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log('MongoDB connection error:', err));
 
-// Example route
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Start server
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
